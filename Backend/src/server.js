77e7e4a8f7022c112204bf26ee6c3d924111bc21
Quoctienha <quoctienha.1509@gmail.com>
2025-10-06 09:48:59 +0700

    prepare for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,17 +4,26 @@ import { connectDB } from "./config/db.js"
 import dotenv from "dotenv"
 import rateLimiter from "./middlewares/rateLimiter.js"
 import cors from 'cors'
+import path from "path"
+
 
 //access enviromnt variables
 dotenv.config()
 
 const app = express()
-const port = 3000
+const port = 3000 
+//const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
+
 
 //middleware
-app.use(cors({
-  origin: "http://localhost:5173"
-}))
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })); // Middleware xử lý dữ liệu từ form (x-www-form-urlencoded)
 app.use(rateLimiter)
@@ -25,11 +34,19 @@ app.use(rateLimiter)
 //ROUTES
 app.use("/api/notes", notesRoutes)
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+  });
+}
+
 //connect to mongoDBs
 connectDB().then(() => {
   //connect to db first and then listening on port
   app.listen(port, () => {
-    console.log(`App listening on port http://localhost:${port}/api/notes`)
+    console.log(`App listening on port http://localhost:${port}`)
   })
 })
 
--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
 [MERN Stack Tutorial for Beginners](https://youtu.be/Ea9rrRj9e0Y?si=DNPCSb7YC6lwtJ0z)
+[Link github of the tutorial](https://github.com/burakorkmez/mern-thinkboard)
